79a5755a3eaea611a2dd79ed929a0c79b44b81f6
maddipsw <33684390+maddipsw@users.noreply.github.com>
2026-04-15 16:02:05 +0530

    Update outputs.tf

--- modules/ec2/outputs.tf
+++ modules/ec2/outputs.tf
@@ -21,22 +21,38 @@ output "computed_names" {
 # Additional EBS volume outputs
 output "ebs_volume_ids" {
   description = "List of additional EBS volume IDs"
-  value       = aws_ebs_volume.additional[*].id
+  value       = { for k, v in aws_ebs_volume.additional : k => v.id }
 }
 
 output "volume_attachment_ids" {
   description = "List of EBS volume attachment IDs"
-  value       = aws_volume_attachment.additional[*].id
+  value       = { for k, v in aws_volume_attachment.additional : k => v.id }
 }
 
 # Drift detection outputs
 output "drift_detection_data" {
   description = "Drift detection data for monitoring (only available when enable_drift_detection = true)"
   value = var.enable_drift_detection ? {
-    instance_states  = data.aws_instance.drift_check[*].instance_state
-    instance_types   = data.aws_instance.drift_check[*].instance_type
-    attached_volumes = data.aws_ebs_volumes.attached_volumes[*].ids
-    security_groups  = data.aws_instance.drift_check[*].vpc_security_group_ids
+    instance_states  = { for k, v in data.aws_instance.drift_check : k => v.instance_state }
+    instance_types   = { for k, v in data.aws_instance.drift_check : k => v.instance_type }
+    attached_volumes = { for k, v in data.aws_ebs_volumes.attached_volumes : k => v.ids }
+    security_groups  = { for k, v in data.aws_instance.drift_check : k => v.vpc_security_group_ids }
   } : null
   sensitive = false
+}
+
+# Combined instance details
+output "instance_details" {
+  description = "Complete details for all created instances"
+  value = {
+    for k, v in aws_instance.this : k => {
+      id                = v.id
+      arn               = v.arn
+      private_ip        = v.private_ip
+      availability_zone = v.availability_zone
+      instance_type     = v.instance_type
+      subnet_id         = v.subnet_id
+      instance_state    = v.instance_state
+    }
+  }
 }
